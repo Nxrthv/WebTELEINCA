dfd4a353b6245d069a8c35a872a636085f776cd3
unknown <vargasesquivelandres@gmail.com>
2024-12-18 12:10:56 -0500

    Andres Vargas

---
+++ Resources/Js/Functions.js
@@ -0,0 +1,19 @@
+function sendMessage(cardId) {
+    const card = document.getElementById(cardId);
+    const paquete = card.querySelector('#paquete').textContent;
+    const velocidad = card.querySelector('#velocidad').textContent;
+    const cable = card.querySelector('#cable').textContent;
+    const precio = card.querySelector('#precio').textContent;
+    
+    const message = `¡Hola! Estoy interesado en el paquete de ${paquete}. Con ${velocidad} de velocidad y ${cable}.\nPrecio: ${precio}`;
+    
+    const phoneNumber = "51962624690";
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    
+    window.open(whatsappUrl, "_blank");
+}
+
+function internet() {
+    window.location.href = "/Views/Internet.html";
+}

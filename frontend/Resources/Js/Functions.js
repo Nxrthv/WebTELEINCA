cc73b22224d99c0e70c8b9c03a7d05c3e1c65c14
unknown <vargasesquivelandres@gmail.com>
2024-12-24 10:16:43 -0500

    Andres Vargas

--- frontend/Resources/Js/Functions.js
+++ frontend/Resources/Js/Functions.js
@@ -14,22 +14,28 @@ function sendMessage(cardId) {
     window.open(whatsappUrl, "_blank");
 }
 
-function internet() {
-    window.location.href = "/Views/Internet.html";
-}
+function smoothScrollTo(sectionId) {
+    const target = document.getElementById(sectionId);
+    const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
 
-window.onscroll = function() {
-    stickyNav();
-  };
-  
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
-  
-  function stickyNav() {
-    if (window.pageYOffset > sticky) {
-      navbar.classList.add("fixed");
-    } else {
-      navbar.classList.remove("fixed");
+    function animation(currentTime) {
+        if (start === null) start = currentTime;
+        const timeElapsed = currentTime - start;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
     }
-  }
-  
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+};
